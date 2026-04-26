fb659966a7b1dc951edcdb7c98be2e55c71bdf80
Szabolcs Kovács <kovacs.szabolcs.p@gmail.com>
2026-04-26 11:50:16 +0200

    Initiation

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace radaway_surcharge_calc_HUN
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
